52ce8a2aae3a15026bbe02e841064fd51691aa39
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-05-05 14:21:41 +0100

    woekexperiance giving error

--- src/app/(main)/editor/steps.ts
+++ src/app/(main)/editor/steps.ts
@@ -2,6 +2,7 @@ import React from "react";
 import GenerateInfoForm from "./forms/generateInfoForm";
 import PersonalInforForm from "./forms/personalInforForm";
 import { EditorFormProps } from "@/lib/types";
+import WorkExperienceForm from "./forms/WorkExperienceForm";
 
 export const steps: {
   title: string;
@@ -9,9 +10,15 @@ export const steps: {
   key: string;
 }[] = [
   { title: "Generate info", component: GenerateInfoForm, key: "generate-info" },
+
   {
     title: "Personal info",
     component: PersonalInforForm,
     key: "personal-info",
+  },
+  {
+    title: "Work Experience",
+    component: WorkExperienceForm,
+    key: "work-experience",
   },
 ];
