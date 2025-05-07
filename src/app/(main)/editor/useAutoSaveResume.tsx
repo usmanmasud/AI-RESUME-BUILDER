b554a56e908b6fcce72b0a4c6015f4847050e433
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-05-07 14:16:37 +0100

    create autosave

---
+++ src/app/(main)/editor/useAutoSaveResume.tsx
@@ -0,0 +1,101 @@
+import { useToast } from "@/hooks/use-toast";
+import useDebounce from "@/hooks/useDebounce";
+import { ResumeValues } from "@/lib/validation";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { saveResume } from "./actions";
+import { Button } from "@/components/ui/button";
+
+export default function useAutoSaveResume(resumeData: ResumeValues) {
+  const searchParams = useSearchParams();
+
+  const { toast } = useToast();
+
+  const debounceResumeData = useDebounce(resumeData, 1500);
+
+  const [resumeId, setResumeId] = useState(resumeData.id);
+
+  const [lastSaveData, setLastSaveData] = useState(structuredClone(resumeData));
+
+  const [isSaving, setIsSaving] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    setIsError(false);
+  }, [debounceResumeData]);
+
+  useEffect(() => {
+    async function save() {
+      try {
+        setIsSaving(true);
+        setIsError(false);
+
+        const newData = structuredClone(debounceResumeData);
+
+        const updateResume = await saveResume({
+          ...newData,
+          ...(lastSaveData.photo?.toString() === newData.photo?.toString() && {
+            photo: undefined,
+          }),
+          id: resumeId,
+        });
+
+        setResumeId(updateResume.id);
+        setLastSaveData(newData);
+
+        if (searchParams.get("resumeId") !== updateResume.id) {
+          const newSearchParams = new URLSearchParams(searchParams);
+          newSearchParams.set("resumeId", updateResume.id);
+          window.history.replaceState(
+            null,
+            "",
+            `?${newSearchParams.toString()}`,
+          );
+        }
+      } catch (error) {
+        setIsError(true);
+        console.error(error);
+        const { dismiss } = toast({
+          variant: "destructive",
+          description: (
+            <div className="space-y-3">
+              <p>Could not save changes.</p>
+              <Button
+                variant="secondary"
+                onClick={() => {
+                  dismiss();
+                  save();
+                }}
+              >
+                Retry
+              </Button>
+            </div>
+          ),
+        });
+      } finally {
+        setIsSaving(false);
+      }
+    }
+
+    const hasUnsavedChanges =
+      JSON.stringify(debounceResumeData) !== JSON.stringify(lastSaveData);
+
+    if (hasUnsavedChanges && debounceResumeData && !isSaving && isError) {
+      save();
+    }
+  }, [
+    debounceResumeData,
+    isSaving,
+    lastSaveData,
+    isError,
+    resumeId,
+    searchParams,
+    toast,
+  ]);
+
+  return {
+    isSaving,
+    hasUnsavedChanges:
+      JSON.stringify(resumeData) !== JSON.stringify(lastSaveData),
+  };
+}
