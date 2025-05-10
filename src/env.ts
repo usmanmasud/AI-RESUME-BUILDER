dbd0e7f738f42dbe6fc5740b44636f9605d29d3b
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-05-11 00:35:41 +0100

    ..

--- src/env.ts
+++ src/env.ts
@@ -15,6 +15,7 @@ export const env = createEnv({
     BLOB_READ_WRITE_TOKEN: z.string().min(1),
     OPENAI_API_KEY: z.string().min(1),
     STRIPE_SECRET_KEY: z.string().min(1),
+    STRIPE_WEBHOOK_SECRET: z.string().min(1),
   },
   client: {
     NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: z.string().min(1),
