6ff78e4dfb70707a8616c3641f95177d2c77b3b1
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-05-10 13:01:57 +0100

    //

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,13 +1,27 @@
 import PremiumModal from "@/components/premium/PremiumModal";
 import Navbar from "./Navbar";
+import { auth } from "@clerk/nextjs/server";
+import { getUserSubscriptionLevel } from "@/lib/subscriptions";
+import SubscriptionLevelProvider from "./SubscriptionLevelProvider";
 
-const layout = ({ children }: { children: React.ReactNode }) => {
+const layout = async ({ children }: { children: React.ReactNode }) => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return null;
+  }
+
+  const userSubscription = await getUserSubscriptionLevel(userId);
+
   return (
-    <div className="flex max-h-screen flex-col">
-      <Navbar />
-      {children}
-      <PremiumModal />
-    </div>
+    <SubscriptionLevelProvider userSubscriptionLevel={userSubscription}>
+      {" "}
+      <div className="flex max-h-screen flex-col">
+        <Navbar />
+        {children}
+        <PremiumModal />
+      </div>
+    </SubscriptionLevelProvider>
   );
 };
 
