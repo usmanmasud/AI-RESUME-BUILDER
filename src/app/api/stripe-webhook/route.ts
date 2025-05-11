ac1fb5cfc66e5161b535aaede953bcc7b22fd66b
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-05-11 10:11:00 +0100

    update webhook route

--- src/app/api/stripe-webhook/route.ts
+++ src/app/api/stripe-webhook/route.ts
@@ -52,7 +52,9 @@ async function handleSessionCompletes(session: Stripe.Checkout.Session) {
     throw new Error("UserId is missing in session metadata");
   }
 
-  (await clerkClient()).users.updateUserMetadata(userId, {
+  await (
+    await clerkClient()
+  ).users.updateUserMetadata(userId, {
     privateMetadata: {
       stripeCustomerId: session.customer as string,
     },
